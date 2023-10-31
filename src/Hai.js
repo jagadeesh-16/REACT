b6d2c3178c502d0c9b77a499030af5ef638860f1
jagadeesh-16 <jagadeeshpalanisamy16@gmail.com>
2023-10-31 11:41:07 +0530

    first commit

---
+++ src/Hai.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+
+export default class Hai extends Component {
+  render() {
+    return (
+      <div>Hai</div>
+    )
+  }
+}
+<>
+<button onClick={myfunc}>Increase</button>
+<span>{counts1}</span>
+<button onClick={myfunc1}>Decrease</button>
+</>
